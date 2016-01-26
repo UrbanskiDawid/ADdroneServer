e7d847c324544dd663e246c1e4d0a0b37e814dfa
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-01-26 22:55:12 +0100

    Indenation corrected

--- ADdroneServer.py
+++ ADdroneServer.py
@@ -19,20 +19,20 @@ decoder = MessageDecoder()
 sender = UartSender()
 
 while True:
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print(sys.stderr, 'client connected:', client_address)
-        while True:
-            data=connection.recv(256)
-            if data:
-              if not decoder.tryToReadMessage(data):
-                 print('received garbage "%s"' % data)
-              sender.sendMessage('Hello, UART!') 
-              connection.sendall(data)
-            else:
-              print('connection closed');
-              connection.close()
-              break;
-    finally:
+  print('waiting for a connection')
+  connection, client_address = sock.accept()
+  try:
+    print(sys.stderr, 'client connected:', client_address)
+    while True:
+      data=connection.recv(256)
+      if data:
+        if not decoder.tryToReadMessage(data):
+          print('received garbage "%s"' % data)
+        sender.sendMessage('Hello, UART!') 
+        connection.sendall(data)
+      else:
+        print('connection closed');
         connection.close()
+        break;
+  finally:
+    connection.close()
--- UartSender.py
+++ UartSender.py
@@ -1,8 +1,8 @@
 import serial
 
 class UartSender:
-	def sendMessage(self, message):
-		connection = serial.Serial ("/dev/ttyAMA0") 
-		connection.baudrate = 9600 
-		connection.write("Hello") 
-		connection.close()
+  def sendMessage(self, message):
+    connection = serial.Serial ("/dev/ttyAMA0") 
+    connection.baudrate = 9600 
+    connection.write("Hello") 
+    connection.close()
