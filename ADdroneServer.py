47b7860961bb417196b6902b00221f1f7d64758c
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-01-26 23:05:15 +0100

    Port as an cmd line  argument

--- ADdroneServer.py
+++ ADdroneServer.py
@@ -9,8 +9,13 @@ from UartSender import *
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the address given on the command line
-server_name = ""#"localhost"
-server_address = (server_name, 1234)
+server_name = "" # "localhost"
+if (len(sys.argv) < 2):
+  print ('Missing port number')
+  sys.exit()
+
+port_number = int(sys.argv[1])
+server_address = (server_name, port_number)
 
 sock.bind(server_address)
 sock.listen(1)
