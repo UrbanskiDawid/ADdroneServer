064469263060cacf97eb2ec3982c14c02fb16a23
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-02-08 22:17:09 +0100

    Logger: added insering new entry in table

--- Logger.py
+++ Logger.py
@@ -1,11 +1,15 @@
 import sqlite3
 
 class Logger:
+  conn = None;
+
   def __init__(self):
-    conn = sqlite3.connect('test.db')
-	conn.execute('''CREATE TABLE COMPANY
+    self.conn = sqlite3.connect('test.db')
+    self.conn.execute('''CREATE TABLE RECEIVED
        (ID INT PRIMARY KEY     NOT NULL,
-       NAME           TEXT    NOT NULL,
-       AGE            INT     NOT NULL,
-       ADDRESS        CHAR(50),
-       SALARY         REAL);''')
+       TIME           INT      NOT NULL,
+       SRC_ADDR       TEXT     NOT NULL);''')
+
+  def logReceivedMessage(self, srcAddr):
+    self.conn.execute('''("INSERT INTO RECEIVED (ID,TIME,SRC_ADD) \
+                           VALUES (0, 0, '0.0.0.0')");''')
