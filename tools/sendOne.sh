84f12d5cc7b93b79e7a700b0f326b2ddfb2d4d9e
Super Dawid <dawid@raspberrypi.(none)>
2016-02-28 15:17:22 +0000

    add help

--- tools/sendOne.sh
+++ tools/sendOne.sh
@@ -1,1 +1,9 @@
-echo -n '$$$$assaaabbbbbbbbbbccccccccccdddddddd' | netcat -q 0 localhost 5555
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+echo "sending single message to port"
+echo " arg1: port "
+exit 1
+fi
+
+echo -n '$$$$assaaabbbbbbbbbbccccccccccdddddddd' > /dev/tcp/localhost/$1
