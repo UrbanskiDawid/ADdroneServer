3fe9102dab28ce48e2217a4383289a74cee50113
ebarnaw <bartosz.nawrot@ericsson.com>
2016-08-17 15:32:23 +0200

    Missing files

---
+++ TestDroneController.py
@@ -0,0 +1,37 @@
+from DroneControler import DroneController
+from ControlData import *
+from UartSender import *
+import time
+from LogWriter import *
+import struct
+
+RB2 = "/dev/ttyAMA0"
+RB3 = "/dev/ttyS0"
+
+def onReceiveUsart(debugData):
+    print 'Recived:', str(debugData)
+
+
+logWriter = LogWriter('logs')
+droneControler = DroneController(RB2, 115200, False, logWriter)
+
+droneControler.setOnReceiveEvent(onReceiveUsart)
+
+# time of test in seconds
+maxTime = 30
+
+i = 0
+while i < maxTime*2:
+    i += 1
+    droneControler.setControlData(ControlData.SomeValidControlCommand())
+    time.sleep(0.5)
+
+i = 0
+while i < 2:
+    i += 1
+    droneControler.setControlData(ControlData.StopCommand())
+    time.sleep(0.5)
+
+droneControler.close()
+
+print "DONE"
